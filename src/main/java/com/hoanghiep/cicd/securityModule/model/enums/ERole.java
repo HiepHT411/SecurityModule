6cdfb88a6c7ae036455d9a183022fa66f515480f
Robert Tran <robert.tuan@cprvision.com>
2023-02-27 18:04:11 +0700

    add security

---
+++ src/main/java/com/hoanghiep/cicd/securityModule/model/enums/ERole.java
@@ -0,0 +1,6 @@
+package com.hoanghiep.cicd.securityModule.model.enums;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/hoanghiep/cicd/securityModule/model/payload/MessageResponse.java
@@ -0,0 +1,14 @@
+package com.hoanghiep.cicd.securityModule.model.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
